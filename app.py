27930c03a76f56f83175d0c170beb7e50a3094d2
ksrx1world <ksrx1world@gmail.com>
2020-06-07 16:40:39 +0530

    edited theme

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import json
 from datetime import datetime,timedelta
 
+
 app = Flask(__name__)
 
 
@@ -19,46 +20,40 @@ class config:
 
 
 
-
+res = boto3.resource('ec2',region_name=config.region)
 ec2 = boto3.client('ec2', region_name=config.region)
 cloudfront = boto3.client('cloudwatch', region_name=config.region)
 
 
-
+response = ec2.describe_instances(InstanceIds=[config.InstanceIds])
+instanceip=response.get('Reservations', [{}])[0].get('Instances',[{}])[0].get('PublicIpAddress')
+instanceid=response.get('Reservations', [{}])[0].get('Instances',[{}])[0].get('InstanceId')
+instancetype=response.get('Reservations', [{}])[0].get('Instances',[{}])[0].get('InstanceType')
+instanceloc=response.get('Reservations', [{}])[0].get('Instances',[{}])[0].get('KeyName')
+volumeid=response.get('Reservations', [{}])[0].get('Instances',[{}])[0].get('BlockDeviceMappings', [{}])[0].get('Ebs').get('VolumeId')
+volumesize=0
 # def instancedetails():
 #     response = ec2.describe_instances(InstanceIds=[config.InstanceIds])
 #     return render_template('index.html', ip=(response.get('Reservations', [{}])[0].get('Instances',[{}])[0].get('PublicIpAddress')))
 
-    
+defvol = res.Volume(volumeid)
+  
 
 
 @app.route('/')
 def cpucredit():
-    credit = cloudfront.get_metric_statistics(
-            Namespace='AWS/EC2',
-            MetricName='CPUCreditBalance',
-            Dimensions=[
-                {
-                'Name': 'InstanceId',
-                'Value': config.InstanceIds
-                },
-            ],
-            StartTime=datetime.utcnow()-timedelta(minutes = 15),
-            EndTime=datetime.utcnow(),
-            Period=900,
-            Statistics=[
-                'Average',
-            ]
-        )
-    return render_template('index.html',score= int(credit.get('Datapoints', [{}])[0].get('Average')))
+    instance = res.Instance(instanceid)
+    volumes = instance.volumes.all()
+    for volume in volumes:
+        volumesize =+ volume.size
+    # volumesize=40       
+    volleft=30-volumesize
+    return render_template('index.html',volumesize=volumesize,volleft=volleft)
     # return (int(credit.get('Datapoints', [{}])[0].get('Average')))
     
 
 @app.route('/details/' , methods=['GET','POST'])
 def instancedetails():
-    response = ec2.describe_instances(InstanceIds=[config.InstanceIds])
-    b=response.get('Reservations', [{}])[0].get('Instances',[{}])[0].get('PublicIpAddress')
-    c=response.get('Reservations', [{}])[0].get('Instances',[{}])[0].get('InstanceId')
     credit = cloudfront.get_metric_statistics(
             Namespace='AWS/EC2',
             MetricName='CPUCreditBalance',
@@ -76,7 +71,7 @@ def instancedetails():
             ]
         )
     d=str(int(credit.get('Datapoints', [{}])[0].get('Average')))+" /144"        
-    jso= { "Instance Id" : c, "Instance Ipv4" : b,"CPU Credits" : d
+    jso= {"CPU Credits" : d,"Instance Id" : instanceid,"Instance Ipv4" : instanceip, "Instance type" : instancetype ,"Instance location" : instanceloc
         } 
     return jsonify(results = jso)
 
@@ -85,8 +80,14 @@ def instdetails():
     response = ec2.describe_instances(InstanceIds=[config.InstanceIds])
     return response
     
+@app.route('/lsl')
+def shows():
+    instance = res.Instance(instanceid)
+    volumes = instance.volumes.all()
+    for volume in volumes:
+        volumesize =+ volume.size
 
-
+        return str(volumesize)
 
 
 
